109d05c0f6a67eb4bf2645b9ffaeb4121d023a1d
Viktor <vintaj97@mail.ru>
2021-02-12 21:32:08 +0300

    lab_3_a_star выполненая 3я лабораторная работа

---
+++ lab_3_a_star/src/main/java/ru/mtusi/Location.java
@@ -0,0 +1,31 @@
+package ru.mtusi;
+
+
+import lombok.EqualsAndHashCode;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+
+@EqualsAndHashCode
+public class Location {
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y) {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location() {
+        this(0, 0);
+    }
+
+}
